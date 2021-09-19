37775444321536ee47abe095149c57685e22652f
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-09-19 15:57:09 -0400

    Add hello Raj function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def hello_world():
     print("hello world!")
 
+def hello_raj():
+    print("hello Raj!")
+
 def goodbye_world():
     print("goodbye world!")
 
 hello_world()
+hello_raj()
 goodbye_world()
